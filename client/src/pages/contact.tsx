2fc052b0d906362199f48df156d5bb1f4d772337
edzeame <38935644-edzeame@users.noreply.replit.com>
2025-01-27 19:11:28 +0000

    Agent query: 1. Is the map showing the correct MSG WAREHOUSE location (5.65387678048915, -0.27810093120146023)? 2. Are the new sea blue and white colors displaying correctly throughout the site?
    Update contact page map embed URL.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/23bc54cf-813e-4a26-8be1-bff20983cf13/919de776-1daa-4083-99db-790fccef5ea7.jpg

--- client/src/pages/contact.tsx
+++ client/src/pages/contact.tsx
@@ -60,7 +60,7 @@ export default function Contact() {
             {/* Map */}
             <div className="aspect-video rounded-lg overflow-hidden">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.087691457245!2d-0.2781009312014602!3d5.653876780489151!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNcKwMzknMTQuMCJOIDDCsDE2JzQxLjIiVw!5e0!3m2!1sen!2sgh!4v1705881600000!5m2!1sen!2sgh"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.087691457245!2d-0.27810093120146023!3d5.65387678048915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNcKwMzknMTQuMCJOIDDCsDE2JzQxLjIiVw!5e0!3m2!1sen!2sgh!4v1705881600000!5m2!1sen!2sgh"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
